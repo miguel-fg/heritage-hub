2e7c4c45ccaac08f1ef0d87f7915228d5f5ac259
miguel-fg <miguelfierro55@gmail.com>
2025-02-01 20:54:23 -0800

    Add base colours & typography

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,9 @@
 
 <template>
   <div>
-    <h1>Heritage Hub</h1>
+    <h1 class="title">This is a Title</h1>
+    <h2 class="subtitle">This is a subtitle</h2>
+    <p class="body">This is body text</p>
+    <div class="tag">This is a tag</div>
   </div>
 </template>
