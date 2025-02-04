5c26150896d8611b20879bab94096ccbe065dec2
miguel-fg <miguelfierro55@gmail.com>
2025-02-04 11:05:16 -0800

    Add dynamic button component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,36 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
     <h1 class="title">This is a Title</h1>
     <h2 class="subtitle">This is a subtitle</h2>
     <p class="body">This is body text</p>
     <div class="tag">This is a tag</div>
+    <div class="flex flex-col gap-4">
+      <Button type="primary" @clicked="testPrimary">
+        <img src="../assets/icons/upload.svg" alt="Upload icon" class="w-3" />
+        <span>Upload</span>
+      </Button>
+      <Button type="secondary" @clicked="testSecondary">
+        <span>Secondary</span>
+      </Button>
+      <Button type="ghost" @clicked="testGhost">
+        <span>Ghost</span>
+      </Button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Button from "./components/Button.vue";
+
+const testPrimary = () => {
+  console.log("Primary test! :D");
+};
+
+const testSecondary = () => {
+  console.log("Secondary test! :)");
+};
+
+const testGhost = () => {
+  console.log("Ghost test! :P");
+};
+</script>
---
+++ client/src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    class="flex grow-0 shrink-0 w-fit items-center gap-1 font-medium font-poppins cursor-pointer rounded-xs"
+    :class="[buttonClass, props.class]"
+    @click="() => emit('clicked')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  type: { type: String, required: true },
+  class: String,
+});
+
+const emit = defineEmits(["clicked"]);
+
+const buttonClass = computed(() => {
+  switch (props.type) {
+    case "primary":
+      return "px-2 py-1 bg-primary-500 text-grayscale-100 drop-shadow-sm";
+    case "secondary":
+      return "px-2 py-1 bg-grayscale-300 text-grayscale-800 drop-shadow-sm";
+    case "ghost":
+      return "px-0 py-0 bg-transparent text-grayscale-700 underline";
+  }
+});
+</script>
