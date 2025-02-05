738775d7e1968c9c2fbdf01f778f013e154b15cb
miguel-fg <miguelfierro55@gmail.com>
2025-02-04 16:31:44 -0800

    Add responsive footer component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,42 @@
 <template>
-  <NavBar />
-  <div>
-    <h1 class="title">This is a Title</h1>
-    <h2 class="subtitle">This is a subtitle</h2>
-    <p class="body">This is body text</p>
-    <div class="tag">This is a tag</div>
-    <div class="flex flex-col gap-4">
-      <Button type="primary" @clicked="testPrimary">
-        <img src="../assets/icons/upload.svg" alt="Upload icon" class="w-3" />
-        <span>Upload</span>
-      </Button>
-      <Button type="secondary" @clicked="testSecondary">
-        <span>Secondary</span>
-      </Button>
-      <Button type="ghost" @clicked="testGhost">
-        <span>Ghost</span>
-      </Button>
+  <div class="flex flex-col h-screen">
+    <NavBar />
+    <div class="grow px-4 md:px-8 lg:px-16">
+      <h1 class="title">This is a Title</h1>
+      <h2 class="subtitle">This is a subtitle</h2>
+      <p class="body">This is body text</p>
+      <div class="tag">This is a tag</div>
+      <div class="flex flex-col gap-4">
+        <Button type="primary" @clicked="testPrimary">
+          <img src="../assets/icons/upload.svg" alt="Upload icon" class="w-3" />
+          <span>Upload</span>
+        </Button>
+        <Button type="secondary" @clicked="testSecondary">
+          <span>Secondary</span>
+        </Button>
+        <Button type="ghost" @clicked="testGhost">
+          <span>Ghost</span>
+        </Button>
+      </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
-import Button from "./components/Button.vue";
-import NavBar from "./components/NavBar.vue";
+  import Button from "./components/Button.vue";
+  import NavBar from "./components/NavBar.vue";
+  import Footer from "./components/Footer.vue";
 
-const testPrimary = () => {
-  console.log("Primary test! :D");
-};
+  const testPrimary = () => {
+    console.log("Primary test! :D");
+  };
 
-const testSecondary = () => {
-  console.log("Secondary test! :)");
-};
+  const testSecondary = () => {
+    console.log("Secondary test! :)");
+  };
 
-const testGhost = () => {
-  console.log("Ghost test! :P");
-};
+  const testGhost = () => {
+    console.log("Ghost test! :P");
+  };
 </script>
---
+++ client/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="flex flex-col p-4 lg:p-8 gap-8 md:px-8 lg:px-16 lg:flex-row lg:justify-between border-b-[1rem] border-primary-500 bg-primary-700">
+    <div class="flex flex-col items-center">
+      <img src="/HH_white.svg" alt="Heritage Hub logo" class="w-20" />
+      <span class="title text-grayscale-100">Heritage Hub</span>
+    </div>
+    <div class="flex justify-between text-grayscale-100 lg:gap-48 xl:gap-64">
+      <div class="max-w-32">
+        <h1 class="subtitle">Navigate</h1>
+        <ul class="flex flex-col gap-2 mt-3 underline">
+          <li>Home</li>
+          <li>About</li>
+          <li>Explore</li>
+        </ul>
+      </div>
+      <div class="max-w-32">
+        <h1 class="subtitle">Actions</h1>
+        <ul class="flex flex-col gap-2 mt-3 underline">
+          <li>Upload</li>
+          <li>Search</li>
+        </ul>
+      </div>
+      <div class="max-w-32">
+        <h1 class="subtitle">Resources</h1>
+        <ul class="flex flex-col gap-2 mt-3 underline">
+          <li class="text-pretty">Museum of Archaeology & Ethnology</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
