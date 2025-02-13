c25280cfd41240fe9d71c993d4f926cd691ed69b
miguel-fg <miguelfierro55@gmail.com>
2025-02-13 12:02:10 -0800

    Add routing hover/active effects

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,13 +1,14 @@
 <template>
   <div
-    class="w-full border-b border-grayscale-300 bg-white px-4 md:px-8 lg:px-16"
+    class="w-full bg-white px-4 md:px-8 lg:px-16"
+    :class="isOpen ? 'border-none' : 'border-b border-grayscale-300'"
   >
     <div class="w-full mx-auto max-w-[1920px]">
       <div class="flex flex-col">
         <div class="flex lg:justify-between items-center py-2 gap-5">
           <div class="flex gap-4 lg:gap-12 grow-1 items-center">
             <img class="w-10" src="/HH_red.svg" alt="Heritage Hub logo" />
-            <Button class="lg:hidden" type="search" @clicked="handleSearch">
+            <Button class="lg:hidden" type="search" @click="handleSearch">
               <img
                 src="../../assets/icons/search.svg"
                 alt="Search icon"
@@ -17,9 +18,36 @@
             </Button>
             <nav class="hidden lg:block">
               <ul class="flex gap-8 bg-white font-poppins font-regular">
-                <li class="hover:underline">Home</li>
-                <li class="hover:underline">Explore</li>
-                <li class="hover:underline">About</li>
+                <li
+                  class="pointer-cursor"
+                  :class="
+                    route.name === 'Home'
+                      ? 'border-b-3 border-primary-500 text-primary-600 font-medium'
+                      : 'border-none hover:underline'
+                  "
+                >
+                  <RouterLink to="/home">Home</RouterLink>
+                </li>
+                <li
+                  class="pointer-cursor"
+                  :class="
+                    route.name === 'About'
+                      ? 'border-b-3 border-primary-500 text-primary-600 font-medium'
+                      : 'border-none hover:underline'
+                  "
+                >
+                  <RouterLink to="/about">About</RouterLink>
+                </li>
+                <li
+                  class="pointer-cursor"
+                  :class="
+                    route.name === 'Gallery'
+                      ? 'border-b-3 border-primary-500 text-primary-600 font-medium'
+                      : 'border-none hover:underline'
+                  "
+                >
+                  <RouterLink to="/">Explore</RouterLink>
+                </li>
               </ul>
             </nav>
           </div>
@@ -27,7 +55,7 @@
             <Button
               class="hidden lg:inline-flex w-xs"
               type="search"
-              @clicked="handleSearch"
+              @click="handleSearch"
             >
               <img
                 src="../../assets/icons/search.svg"
@@ -36,7 +64,7 @@
               />
               <span>Search</span>
             </Button>
-            <Button type="primary" class="px-3" @clicked="handleUpload">
+            <Button type="primary" class="px-3" @click="handleUpload">
               <img
                 src="../../assets/icons/upload.svg"
                 alt="Upload icon"
@@ -44,7 +72,7 @@
               />
               <span class="hidden md:block">Upload</span>
             </Button>
-            <Button type="ghost" @clicked="toggleNav" class="lg:hidden">
+            <Button type="ghost" @click="toggleNav" class="lg:hidden">
               <img
                 v-if="!isOpen"
                 src="../../assets/icons/menu.svg"
@@ -59,24 +87,52 @@
             </Button>
           </div>
         </div>
-        <nav class="lg:hidden" v-show="isOpen">
-          <ul
-            class="flex flex-col gap-2 pt-2 bg-white font-poppins font-regular"
-          >
-            <li class="pl-4 py-2 hover:bg-primary-100/50">Home</li>
-            <li class="pl-4 py-2 hover:bg-primary-100/50">Explore</li>
-            <li class="pl-4 py-2 hover:bg-primary-100/50">About</li>
-          </ul>
-        </nav>
       </div>
     </div>
   </div>
+
+  <nav class="lg:hidden border-b border-grayscale-300" v-show="isOpen">
+    <ul class="flex flex-col gap-2 pt-2 bg-white font-poppins font-regular">
+      <li
+        class="py-2"
+        :class="
+          route.name === 'Home'
+            ? 'bg-primary-100/50 pl-3 border-l-4 border-primary-500 text-primary-600 font-medium'
+            : 'pl-4 border-none'
+        "
+      >
+        <RouterLink to="/home">Home</RouterLink>
+      </li>
+      <li
+        class="py-2"
+        :class="
+          route.name === 'About'
+            ? 'bg-primary-100/50 pl-3 border-l-4 border-primary-500 text-primary-600 font-medium'
+            : 'pl-4 border-none'
+        "
+      >
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+      <li
+        class="py-2"
+        :class="
+          route.name === 'Gallery'
+            ? 'bg-primary-100/50 pl-3 border-l-4 border-primary-500 text-primary-600 font-medium'
+            : 'pl-4 border-none'
+        "
+      >
+        <RouterLink to="/">Explore</RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRoute, RouterLink } from "vue-router";
 import Button from "./Button.vue";
 
+const route = useRoute();
 const isOpen = ref(false);
 
 const toggleNav = () => {
