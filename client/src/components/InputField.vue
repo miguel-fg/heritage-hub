440c9fe4da9f18224081fbc130bd043a967a8da6
miguel-fg <miguelfierro55@gmail.com>
2025-03-04 15:35:30 -0800

    Add ModelForm component

---
+++ client/src/components/InputField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col w-full">
+    <label :for="props.fieldId" class="subtitle text-primary-500">{{
+      props.label
+    }}</label>
+    <input
+      type="text"
+      :id="props.fieldId"
+      class="px-2 py-1 bg-white rounded-xs body border border-grayscale-300"
+    />
+    <span></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  fieldId: { type: String, required: true },
+  label: { type: String, required: true },
+});
+</script>
--- client/src/pages/Upload.vue
+++ client/src/pages/Upload.vue
@@ -1,49 +1,24 @@
 <template>
   <div class="flex gap-5 mx-auto mt-28 mb-32 max-w-[1920px]">
-    <div v-if="fileSelected">
-      <h1 class="title text-grayscale-900">File Name: {{ file.name }}</h1>
-      <h2 class="subtitle text-grayscale-900">File Size: {{ file.size }}</h2>
-      <h2 class="subtitle text-grayscale-900">
-        File Extention: {{ file.extention }}
-      </h2>
-      <Button type="secondary" @click="clearFile">Re-upload Model</Button>
+    <div v-if="!fileSelected" class="flex gap-8 w-full">
+      <div class="flex flex-col w-1/3">
+        <h1 class="title text-grayscale-900">File Name: {{ file.name }}</h1>
+        <h2 class="subtitle text-grayscale-900">File Size: {{ file.size }}</h2>
+        <h2 class="subtitle text-grayscale-900">
+          File Extention: {{ file.extention }}
+        </h2>
+        <Button type="secondary" @click="clearFile">Re-upload Model</Button>
+      </div>
+      <div class="flex flex-col gap-6 w-2/3">
+        <ModelForm />
+      </div>
     </div>
     <div v-else class="w-full flex flex-col justify-center">
-      <div
-        @dragenter.prevent="toggleActive"
-        @dragleave.prevent="toggleActive"
-        @dragover.prevent
-        @drop.prevent="handleDrop"
-        :class="active ? 'bg-grayscale-300 border-grayscale-500' : ''"
-        class="flex justify-center items-center w-full h-148 px-3 border-2 border-grayscale-400 bg-grayscale-200 rounded-xs hover:bg-grayscale-300 hover:border-grayscale-500"
-      >
-        <label
-          class="flex flex-col gap-3 w-full h-full justify-center items-center cursor-pointer"
-          for="objectInput"
-        >
-          <img
-            src="../../assets/icons/object-placeholder.svg"
-            alt="Object placeholder icon"
-          />
-          <p class="font-poppins text-grayscale-800 text-pretty text-center">
-            Click to choose a <span class="font-bold">.glb / .gltf</span> file
-            or drag and drop it here.
-          </p>
-        </label>
-        <input
-          type="file"
-          name="object"
-          id="objectInput"
-          accept=".glb"
-          class="hidden"
-          @change="(e) => handleFileChange(e)"
-        />
-      </div>
-
-      <div class="flex gap-3 w-full justify-end mt-4">
-        <Button type="secondary" @click="cancelUpload">Cancel</Button>
-        <Button type="primary" @click="uploadClick">Upload</Button>
-      </div>
+      <Dropzone
+        v-model="file"
+        @update="() => (fileSelected = true)"
+        @cancel="cancelUpload"
+      />
     </div>
   </div>
 </template>
@@ -52,12 +27,8 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Button from "../components/Button.vue";
-
-const active = ref(false);
-
-const toggleActive = () => {
-  active.value = !active.value;
-};
+import Dropzone from "../components/Dropzone.vue";
+import ModelForm from "../components/ModelForm.vue";
 
 const fileSelected = ref(false);
 
@@ -66,49 +37,6 @@ const file = ref({
   size: 0,
   extention: "",
 });
-
-const handleFileChange = (e: Event) => {
-  const target = e.target as HTMLInputElement;
-  if (target.files && target.files[0]) {
-    const targetFile = target.files[0];
-
-    const fileSize = Math.round((targetFile.size / 1024 / 1024) * 100) / 100;
-    const fileExtention = targetFile.name.split(".").pop();
-    const fileName = targetFile.name.split(".").shift();
-
-    file.value.name = fileName || "";
-    file.value.size = fileSize;
-    file.value.extention = fileExtention || "";
-
-    fileSelected.value = true;
-  }
-};
-
-const handleDrop = (e: DragEvent) => {
-  toggleActive();
-
-  if (e.dataTransfer && e.dataTransfer.files[0]) {
-    const targetFile = e.dataTransfer.files[0];
-
-    const fileSize = Math.round((targetFile.size / 1024 / 1024) * 100) / 100;
-    const fileExtention = targetFile.name.split(".").pop();
-    const fileName = targetFile.name.split(".").shift();
-
-    file.value.name = fileName || "";
-    file.value.size = fileSize;
-    file.value.extention = fileExtention || "";
-
-    fileSelected.value = true;
-  }
-};
-
-const uploadClick = () => {
-  const fileInput = document.getElementById("objectInput");
-
-  if (fileInput) {
-    fileInput.click();
-  }
-};
 
 const clearFile = () => {
   fileSelected.value = false;
