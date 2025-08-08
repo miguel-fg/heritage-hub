c698b366d84a57092f8687493f14559406f3e954
miguel-fg <miguelfierro55@gmail.com>
2025-08-08 12:36:47 -0700

    Remove testing pagination controls

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="mt-20">
     <h1 class="title">Home Page</h1>
-    <PaginationControls />
   </div>
 </template>
 
-<script setup lang="ts">
-import PaginationControls from "../components/PaginationControls.vue";
-</script>
+<script setup lang="ts"></script>
