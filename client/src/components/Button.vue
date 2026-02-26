b4fcfceedefe9168ca5ec0b0c1440777603e5b5f
miguel-fg <miguelfierro55@gmail.com>
2026-02-26 15:49:33 -0800

    Update Model page

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -9,39 +9,39 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { type ButtonType } from "../types/ui";
+import { computed } from 'vue'
+import { type ButtonType } from '../types/ui'
 
 const props = defineProps<{
-  type: ButtonType;
-  class?: string;
-  disabled?: boolean;
-}>();
+  type: ButtonType
+  class?: string
+  disabled?: boolean
+}>()
 
 const buttonClass = computed(() => {
   switch (props.type) {
-    case "primary":
-      return "px-2 py-1 bg-primary-500 text-grayscale-100 drop-shadow-sm hover:bg-primary-600 active:bg-primary-800";
-    case "secondary":
-      return "px-2 py-1 bg-grayscale-200 text-grayscale-800 drop-shadow-sm hover:bg-grayscale-300 active:bg-grayscale-500 active:text-grayscale-900";
-    case "success":
-      return "px-2 py-1 bg-success-700 text-grayscale-100 drop-shadow-sm hover:bg-success-600 hover:text-white active:bg-success-300 active:text-success-900";
-    case "danger":
-      return "px-2 py-1 bg-danger-200 text-danger-800 drop-shadow-sm hover:bg-danger-300 active:bg-danger-700 active:text-danger-100";
-    case "ghost":
-      return "px-0 py-0 bg-transparent text-grayscale-500 hover:text-grayscale-800 hover:underline active:font-bold active:text-primary-500 active:underline";
-    case "ghost-icon":
-      return "px-0 py-1 bg-transparent text-grayscale-700 hover:text-grayscale-900 hover:underline active:font-bold active:text-primary-500 active:underline";
-    case "search":
-      return "px-2 py-1 bg-transparent border border-grayscale-300 text-grayscale-500 grow-1";
-    case "outline":
-      return "px-2 py-1 bg-transparent border border-primary-300 text-primary-500 hover:bg-grayscale-200 active:bg-primary-500 active:text-grayscale-100";
+    case 'primary':
+      return 'px-2 py-1 bg-primary-500 text-grayscale-100 drop-shadow-sm hover:bg-primary-600 active:bg-primary-800'
+    case 'secondary':
+      return 'px-2 py-1 bg-grayscale-200 text-grayscale-800 drop-shadow-sm hover:bg-grayscale-300 active:bg-grayscale-500 active:text-grayscale-900'
+    case 'success':
+      return 'px-2 py-1 bg-success-700 text-grayscale-100 drop-shadow-sm hover:bg-success-600 hover:text-white active:bg-success-300 active:text-success-900'
+    case 'danger':
+      return 'px-2 py-1 bg-danger-200 text-danger-800 drop-shadow-sm hover:bg-danger-300 active:bg-danger-700 active:text-danger-100'
+    case 'ghost':
+      return 'px-0 py-0 bg-transparent text-grayscale-500 hover:text-grayscale-800 active:font-bold active:text-primary-500'
+    case 'ghost-icon':
+      return 'px-0 py-1 bg-transparent text-grayscale-700 hover:text-grayscale-900 hover:underline active:font-bold active:text-primary-500 active:underline'
+    case 'search':
+      return 'px-2 py-1 bg-transparent border border-grayscale-300 text-grayscale-500 grow-1'
+    case 'outline':
+      return 'px-2 py-1 bg-transparent border border-primary-300 text-primary-500 hover:bg-grayscale-200 active:bg-primary-500 active:text-grayscale-100'
   }
-});
+})
 
 const disabledClass = computed(() => {
   return props.disabled
-    ? "opacity-50 cursor-not-allowed pointer-events-none"
-    : "";
-});
+    ? 'opacity-50 cursor-not-allowed pointer-events-none'
+    : ''
+})
 </script>
---
+++ client/src/components/ModelPageToolbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex w-full justify-between items-center relative">
+    <Button type="ghost" @click="emit('back')">
+      <Icon icon="bx:arrow-back" width="20" height="20" />
+      Back
+    </Button>
+    <Button
+      v-if="hasPermissions && modelLoaded"
+      @click="toggleOptionsOpen"
+      type="ghost"
+      class="sm:hidden"
+    >
+      Options
+      <Icon
+        v-if="!isOptionsOpen"
+        icon="bx:chevron-down"
+        width="20"
+        height="20"
+      />
+      <Icon v-if="isOptionsOpen" icon="bx:chevron-up" width="20" height="20" />
+    </Button>
+    <div
+      v-if="hasPermissions && modelLoaded"
+      class="hidden sm:flex gap-8 items-center"
+    >
+      <Button type="ghost" @click="emit('edit')">
+        <Icon icon="bx:edit" width="20" height="20" />
+        Edit
+      </Button>
+      <Button type="ghost" @click="emit('attach')">
+        <Icon icon="bx:paperclip" width="20" height="20" />
+        Attach
+      </Button>
+      <Button type="ghost" @click="emit('delete')">
+        <Icon icon="bx:trash" width="20" height="20" />
+        Delete
+      </Button>
+    </div>
+    <div
+      v-if="isOptionsOpen"
+      class="absolute right-1 top-full z-50 flex flex-col bg-white shadow-sm rounded-sm mt-1"
+    >
+      <Button type="ghost" @click="emit('edit')" class="px-6 py-3">
+        <Icon icon="bx:edit" width="20" height="20" />
+        Edit
+      </Button>
+      <Button type="ghost" @click="emit('attach')" class="px-6 py-3">
+        <Icon icon="bx:paperclip" width="20" height="20" />
+        Attach
+      </Button>
+      <Button type="ghost" @click="emit('delete')" class="px-6 py-3">
+        <Icon icon="bx:trash" width="20" height="20" />
+        Delete
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from './Button.vue'
+import { Icon } from '@iconify/vue'
+import { ref } from 'vue'
+
+const emit = defineEmits(['back', 'edit', 'attach', 'delete'])
+const props = defineProps<{
+  hasPermissions: boolean
+  modelLoaded: boolean
+}>()
+
+const isOptionsOpen = ref(false)
+
+const toggleOptionsOpen = () => (isOptionsOpen.value = !isOptionsOpen.value)
+</script>
